068bdffd2e430d6314332c6b3c9c88fa5537022d
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-13 05:13:34 +0300

    Calculator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,29 @@
 
 
-const val APPLES= 50
+
 fun main(args: Array<String>) {
+    println("Enter first number")
+    var num1 = readLine()!!.toInt()
+    println("Enter second number")
+   var num2 =readLine()!!.toInt()
+    println("Choose one of the operation : + , * , -, / ")
+    var operation = readLine()?.get(0)
 
+    if(operation == '/' && num2 == 0){
+       println("Error cant divide on ZERO!! \n Please enter another number")
+       num2 = readLine()!!.toInt() }
 
-    val numberofDozens= APPLES / 12
-    val remaindofApples=  APPLES % 12
-
-
-        println(numberofDozens)
-       println(remaindofApples)
-
-
-
-
+    when(operation){
+       '-' -> println("Result is " +(num1 - num2))
+       '+' -> println("Result is "+(num1 + num2))
+       '*' -> println("Result is "+(num1 * num2))
+       '/' -> println("Result is "+(num1 / num2))
+   }
 
 }
+
+
+
 
 
 
